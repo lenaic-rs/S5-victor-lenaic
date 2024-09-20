fc7d2639383ad8c5c3a074eaffefb1c0344a4f7b
VSchwarzy <schwartzmannvictor@gmail.com>
2024-09-20 11:18:55 +0200

    ff

--- ALM/TP1/exercice4.2.c
+++ ALM/TP1/exercice4.2.c
@@ -2,15 +2,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int present(int k, int *c) {
-    return c[k] == 1;
+int present(int k, uint32_t *c){ // l'entier k "est dans" le crible c
+ 
+    uint16_t m = 1 << (k % 32);
+    return (c[k / 32] & m) != 0;
 }
 
-void suppression(int k, int *c) {
-    c[k] = 0;
+void suppression(int k, uint32_t *x) {
+
+    uint16_t m = 1 << k;
+    *x = (*x & ~m) | 0 << k;
 }
 
-void construit_crible(int *c, int n) {
+void construit_crible(uint32_t *c, int borne, int n) {
+    // toodo
     for (int i = 0; i < n; i++) {
         c[i] = 1;
     }
@@ -28,15 +33,15 @@ void construit_crible(int *c, int n) {
 }
 
 int main(int argc, char **argv) {
-int *crible, x, k;
+int x, borne;
+uint32_t *crible;
 if (argc == 2) {
 x = atoi(argv[1]);
-crible = (int *)malloc(x*sizeof(int));
-construit_crible(crible, x);
+borne = x/32 + 1;
+crible = (uint32_t *)malloc(borne*sizeof(uint32_t));
+construit_crible(crible, borne, x);
 printf("Nombres premiers jusqu'a %d : \n", x);
-for(k=0; k<x; k++)
-if (present(k,crible)) printf("%d, ", k);
-printf("\n");
+// mettre instructions pour affichage...
 }
 else {
 fprintf(stderr, "./crible <nombre>\n");
