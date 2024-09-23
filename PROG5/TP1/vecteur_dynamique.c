3247306826468588e05f7e29d6dbc4b68543fcf2
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-09-23 16:46:00 +0200

    hh

--- PROG5/TP1/vecteur_dynamique.c
+++ PROG5/TP1/vecteur_dynamique.c
@@ -1,34 +1,70 @@
 #include "vecteur_dynamique.h"
 #include <stdlib.h>
+#include <math.h>
+#include <assert.h>
 
-vecteur allouer_vecteur(int taille) {
-    vecteur v = (vecteur) malloc(sizeof(vecteur));
+vecteur allouer_vecteur(int taille)
+{
+    assert(taille >= 0);
+
+    vecteur v = (vecteur)malloc(sizeof(*v));
+
+    if (v == NULL) {
+        return NULL;
+    }
+
     v->taille = taille;
     v->capacite = taille;
     v->donnees = malloc(taille * sizeof(double));
-    if (v->donnees == NULL) {
+    if (v->donnees == NULL)
+    {
         v->taille = -1;
     }
     return v;
 }
 
-void liberer_vecteur(vecteur v) {
+void liberer_vecteur(vecteur v)
+{
     free(v->donnees);
+    free(v);
 }
 
-int est_vecteur_invalide(vecteur v) {
+int est_vecteur_invalide(vecteur v)
+{
     int resultat = v->taille == -1;
     return resultat;
 }
 
-double *acces_vecteur(vecteur v, int i) {
-    if (i > v->capacite) {
-        v->donnees = realloc(v->donnees, i * sizeof(double));
-        v->capacite = i;
+double *acces_vecteur(vecteur v, int i)
+{
+    if (i >= 0)
+    {
+        if (i >= v->capacite)
+        {
+            if (v->donnees == NULL) {
+                // v->donnees = malloc((i + 1) * sizeof(double));
+                return NULL;
+            } else {
+
+                double* t;
+
+                if (NULL != (t = realloc(v->donnees,  (i + 1) * sizeof(double)))) {
+                    v->donnees = t;
+                } else {
+                    return NULL;
+                }
+                
+            }
+            
+            v->capacite = i + 1;
+            v->taille = i + 1;
+        }
+        return &v->donnees[i];
     }
-    return &v->donnees[i];
+    return NULL;
 }
 
-int taille_vecteur(vecteur v) {
+int taille_vecteur(vecteur v)
+{
     return v->taille;
 }
