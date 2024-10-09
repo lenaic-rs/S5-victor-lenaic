c9f3695591915012039737fffc78023546218c94
VSchwarzy <schwartzmannvictor@gmail.com>
2024-10-09 15:16:32 +0200

    fff

--- PROG5/APNEE_shell/corbeille_v3.sh
+++ PROG5/APNEE_shell/corbeille_v3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 if [[ ! -d "$HOME/.corbeille" ]]
 then
@@ -7,10 +7,42 @@ fi
 
 if [[ $1 == "efface" ]]
 then
+    search=0
+    if [[ $2 == "-r" ]]
+    then
+        search=1
+        shift
+    fi
     for fichier in "${@:2}"
     do
-        mv "$fichier" "$HOME/.corbeille/"
+        if [[ $search -eq 1 ]]
+        then
+            # https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
+            array=()
+            while IFS=  read -r -d $'\0'; do
+                array+=("$REPLY")
+            done < <(find . -name "${fichier}" -print0)
+
+            case ${#array[@]} in
+                0)
+                    echo pas trouvé
+                    ;;
+                1)
+                    mv "${array[0]}" "$HOME/.corbeille/"
+                    ;;
+            
+                *)
+                    for i in "${!array[@]}"; do
+                        mv "${array[$i]}" "$HOME/.corbeille/"
+                    done
+                    ;;
+            esac
+            
+        else
+            mv "$fichier" "$HOME/.corbeille/"
+        fi
     done
+
 elif [[ $1 == "restaure" ]]
 then
     for fichier in "${@:2}"
@@ -47,3 +79,5 @@ then
 else
     echo "Erreur d'argument"
 fi
+
+
