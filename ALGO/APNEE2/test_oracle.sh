1f1b5c3439a1670f74586900bccba20c7d485414
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-10-18 16:59:46 +0200

    oracle

--- ALGO/APNEE2/test_oracle.sh
+++ ALGO/APNEE2/test_oracle.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Ce script utilisera le programme "a.out" pour générer 
-# une chaine aléatoire.
+# Ce script utilisera les chaines contenu dans les fichiers du repertoire supp/tests/
 # Il executera tous les programmes present dans le repertoire
 # "bons/programs"
 # Grace a l'oracle, il verifira si le resultat est le bon, et supprimera le program si
@@ -12,39 +11,46 @@ declare -A table_mauvais
 declare -A different_bug
 
 dir="bons/programs"
+test_dir ="supp/tests"
 lines=$(ls -l $dir | wc -l)
 lines=$(expr $lines - 1)
 
 for i in $(seq 1 1)
 do
-    input=$(./a.out $1)
 
-    for program in $dir/*
+    for test_file in $test_dir/*
     do
-        #echo $program
-        if [ -e $program ]
-        then
-            output=$(echo $input |./$program)
+        input=$(cat $test_file)
 
-            echo $output > tmp
-            echo $input > tmp2
-
-            ./oracle tmp2 tmp
-
-            res=$?
-
-            echo $res
-
-            if [[ $res -ne 0 ]]
+        for program in $dir/*
+        do
+            #echo $program
+            if [ -e $program ]
             then
-                echo $output
-                table_mauvais[$program]=$program
-                #different_bug[$bon]=$bon
-                echo "Error in $program: $input"
-                rm $program || true 
+                output=$(echo $input |./$program)
+
+                echo $output > tmp
+                echo $input > tmp2
+
+                ./oracle tmp2 tmp
+
+                res=$?
+
+                echo $res
+
+                if [[ $res -ne 0 ]]
+                then
+                    echo $output
+                    table_mauvais[$program]=$program
+                    #different_bug[$bon]=$bon
+                    echo "Error in $program: $input"
+                    rm $program || true 
+                fi
             fi
-        fi
+        done
     done
+
+    
 done
 
 
